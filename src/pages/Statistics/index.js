b9dd74da84e432d3967dae4e35e6c73dc15eb1a7
dkcamargox <camargo.douglas@icloud.com>
2021-08-19 14:06:35 -0300

    changing charts package, making it work

--- src/pages/Statistics/index.js
+++ src/pages/Statistics/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
-import { Pie } from "react-chartjs-2";
+
+import { PieChart, Pie, Legend, Cell, Tooltip } from 'recharts';
 
 import Auth from "../../components/Auth";
 import FormContainer from "../../components/FormContainer";
@@ -10,10 +11,26 @@ import api from "../../services/api";
 
 import "./statistics.css";
 
+const RADIAN = Math.PI / 180;
+
 export default class Statistics extends Component {
   state = {
+    data: [
+      { name: 'Group A', value: 400, color: '#0275dbbb' },
+      { name: 'Group B', value: 300, color: '#5cb85cbf' },
+      { name: 'Group C', value: 300, color: '#d9534fbf' },
+      { name: 'Group D', value: 200, color: '#f0ad4ebf' },
+    ]
   };
 
+  getTotalData = () => {
+    const { data } = this.state;
+    const countage = data.map( el => el.value ).reduce((accumulator, value) => (accumulator + value));
+    console.log(countage);
+    return countage;
+
+  }
+  
   renderError = (errorMessage) => {
     this.setState({ error: errorMessage });
     setTimeout(() => {
@@ -22,7 +39,21 @@ export default class Statistics extends Component {
   };
 
 
+  CustomTooltip = (props) => {
+    if (props.active && props.payload && props.payload.length) {
+      const percentage = Math.round((props.payload[0].value / this.getTotalData() + Number.EPSILON) * 10000) / 100;
+      return (
+        <div className="custom-tooltip" style={{ background: '#E5E5E8EE'}}>
+          <p className="label">{`${props.payload[0].name}: ${percentage}%`}</p>
+        </div>
+      );
+    }
+  
+    return null;
+  };
+
   render() {
+    const { CustomTooltip } = this;
     return (
       <>
         <Header />
@@ -33,41 +64,41 @@ export default class Statistics extends Component {
             <h2>Estatisticas</h2>
             <hr />
             <h3>Quantidad de Relevamientos:</h3>
-            <div
-              style={{
-                position: `relative`,
-                width: "95%"
-              }}
-            >
-              <Pie
-                data={{
-                  labels: ['Gaston', 'Manuel', 'Gustavo'],
-                  datasets: [
-                    {
-                      label: '#% of Votes',
-                      data: [5, 5, 3],
-                      backgroundColor: [
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)',
-                      ],
-                      borderColor: [
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)',
-                      ],
-                      borderWidth: 1,
-                    },
-                  ],
-                }}
-              />
 
+            <div
+               style={{
+                 display: 'flex',
+                 flexDirection: 'column',
+                 alignItems: 'center',
+                 justifyContent: 'center'
+
+              }} 
+            >
+            
+              <PieChart width={400} height={400}>
+              
+                <Pie
+                  data={this.state.data}
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={128}
+                  dataKey="value"
+                  blendStroke="true"
+                  isAnimationActive={false}
+                >
+                
+                  {this.state.data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={entry.color}/>
+                  ))}
+                
+                </Pie>
+                
+                
+                <Tooltip content={<CustomTooltip/>}/>
+                <Legend />
+
+                
+              </PieChart>
             </div>
           </main>
         </FormContainer>
